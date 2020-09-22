a26d8e3408725aa038217c987b89fec574aa7d78
kankanshukashu <1411607509@qq.com>
2020-09-22 22:30:52 +0800

    Signed-off-by: kankanshukashu <1411607509@qq.com>

--- src/APIs/bot.js
+++ src/APIs/bot.js
@@ -25,3 +25,51 @@ export const getAllBot = () => {
     url: `/bot/getAll`
   })
 }
+
+export const getBot = (botId) => {
+  return axios({
+    method: 'GET',
+    url: `/bot/info?botId=${botId}`
+  })
+}
+
+export const startBot = (botId) => {
+  return axios({
+    method: 'POST',
+    url: `/bot/start`,
+    data: QS({
+      botId: botId
+    })
+  })
+}
+
+export const stopBot = (botId) => {
+  return axios({
+    method: 'POST',
+    url: `/bot/stop`,
+    data: QS({
+      botId: botId
+    })
+  })
+}
+
+export const deleteBot = (botId) => {
+  return axios({
+    method: 'POST',
+    url: `/bot/delete`,
+    data: QS({
+      botId: botId
+    })
+  })
+}
+
+export const uploadCode = (botId, code) => {
+  return axios({
+    method: 'POST',
+    url: `/bot/uploadCode`,
+    data: QS({
+      botId: botId,
+      code: code
+    })
+  })
+}
